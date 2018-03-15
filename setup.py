4f0f313e907b1ae1e56ccc63c39061a506eb1c6b
Pavel Komarov <pvlkmrv@gmail.com>
2018-03-15 16:39:45 -0400

    removed manifest because it is evidently not really needed and moved requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
 setup(name='projection-pursuit',
-      version='0.5',
+      version='0.6',
       description='Scikit-learn estimators based on projection pursuit.',
       url='https://github.com/pavelkomarov/projection-pursuit',
       author='Pavel Komarov',
       license='BSD',
       packages=[x for x in find_packages() if 'tests' not in x],
-      install_requires=INSTALL_REQUIRES,
+      install_requires=['pytest', 'scikit-learn', 'numpy', 'matplotlib'],
       author_email='pvlkmrv@gmail.com')
