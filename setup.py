adafd16c6e3679f7353e36580f08d3dcbbb32689
Pavel Komarov <pvlkmrv@gmail.com>
2018-03-19 13:35:59 -0400

    setup's version should reflect that in __init__.py automatically now

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 
 from setuptools import setup, find_packages
+import sys
+sys.path.append("skpp")
+import skpp
 
 with open('README.md') as readme:
 	long_description = readme.read()
 
 setup(name='projection-pursuit',
-      version='0.6',
+      version=skpp.__version__,
       description='Scikit-learn estimators based on projection pursuit.',
 	  long_description=long_description,
       url='https://github.com/pavelkomarov/projection-pursuit',
