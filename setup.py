66a041a238d6e3cc9f302c007bfed455c3341995
Pavel Komarov <pvlkmrv@gmail.com>
2018-03-19 13:32:35 -0400

    setup.py now has a long_description, should improve pypi page

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md') as readme:
+	long_description = readme.read()
+
 setup(name='projection-pursuit',
       version='0.6',
       description='Scikit-learn estimators based on projection pursuit.',
+	  long_description=long_description,
       url='https://github.com/pavelkomarov/projection-pursuit',
       author='Pavel Komarov',
       license='BSD',
